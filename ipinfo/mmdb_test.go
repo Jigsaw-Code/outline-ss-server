07a5c20b4a2dd82765f2098a3cadaa3e77ffd572
Vinicius Fortuna <fortuna@users.noreply.github.com>
2023-05-30 17:38:41 -0400

    Use GeoLite2 and add test

--- ipinfo/mmdb_test.go
+++ ipinfo/mmdb_test.go
@@ -16,16 +16,47 @@ package ipinfo
 
 import (
 	"net"
+	"os"
 	"testing"
 
 	"github.com/oschwald/geoip2-golang"
 	"github.com/stretchr/testify/require"
 )
 
-func BenchmarkNewMMDBIPInfoMap(b *testing.B) {
-	var ipCountryDB *geoip2.Reader
+func ensureTestData(tb testing.TB, dbPath string) {
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		tb.Skip("Test MMDB not found. Run git submodule update --init --depth=1")
+	}
+	require.NoError(tb, err)
+}
+
+func TestIPInfoMap(t *testing.T) {
 	// The test data is in a git submodule that must be initialized before running the test.
-	dbPath := "../third_party/maxmind/test-data/GeoIP2-Country-Test.mmdb"
+	dbPath := "../third_party/maxmind/test-data/GeoLite2-Country-Test.mmdb"
+	ensureTestData(t, dbPath)
+	ipCountryDB, err := geoip2.Open(dbPath)
+	require.NoError(t, err, "Could not open geoip database at %v: %v", dbPath, err)
+	defer ipCountryDB.Close()
+	ip2info := NewMMDBIPInfoMap(ipCountryDB)
+
+	info, err := ip2info.GetIPInfo(net.ParseIP("217.65.48.1"))
+	require.NoError(t, err)
+	require.Equal(t, IPInfo{CountryCode: "GI"}, info)
+
+	info, err = ip2info.GetIPInfo(net.ParseIP("127.0.0.1"))
+	require.NoError(t, err)
+	require.Equal(t, IPInfo{CountryCode: ""}, info)
+
+	info, err = ip2info.GetIPInfo(net.ParseIP("2001:250::"))
+	require.NoError(t, err)
+	require.Equal(t, IPInfo{CountryCode: "CN"}, info)
+}
+
+func BenchmarkNewMMDBIPInfoMap(b *testing.B) {
+	// The test data is in a git submodule that must be initialized before running the test.
+	dbPath := "../third_party/maxmind/test-data/GeoLite2-Country-Test.mmdb"
+	ensureTestData(b, dbPath)
 	ipCountryDB, err := geoip2.Open(dbPath)
 	require.NoError(b, err, "Could not open geoip database at %v: %v", dbPath, err)
 	defer ipCountryDB.Close()
